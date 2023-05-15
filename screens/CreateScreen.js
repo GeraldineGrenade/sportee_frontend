57079d8dd63af417b0297611e4360f015097c159
Camille <Your email>
2023-05-15 18:09:29 +0200

    save styleSheet + createScreen

--- screens/CreateScreen.js
+++ screens/CreateScreen.js
@@ -2,6 +2,9 @@ import React from 'react'
 import { SafeAreaView, View, Text, StyleSheet } from 'react-native'
 import FontAwesome from 'react-native-vector-icons/FontAwesome';
 
+
+
+
 const CreateScreen = ({navigation}) => {
     return (
         <SafeAreaView style={styles.container}>
