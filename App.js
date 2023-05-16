b98aa10e1506fa549130d4d7bdd77dcc208657ad
Dorian <dorianlelarge@gmail.com>
2023-05-16 18:02:41 +0200

    google & facebook login with auth

--- App.js
+++ App.js
@@ -10,13 +10,13 @@ import ProfilScreen from './screens/ProfilScreen';
 import SignupJoinScreen from './screens/SignupJoinScreen';
 import SignUpPreferencesScreen from './screens/SignupPreferencesScreen';
 import FontAwesome from 'react-native-vector-icons/FontAwesome';
-
+import Connection from './components/Connection'
 import { Provider } from 'react-redux';
 import { configureStore } from '@reduxjs/toolkit';
 import user from './reducers/user'
 
 const store = configureStore({
- reducer: { user },
+  reducer: { user },
 });
 
 
@@ -28,7 +28,7 @@ const TabNavigator = () => {
     <Tab.Navigator initialRouteName='Recherche' screenOptions={({ route }) => ({
       tabBarIcon: ({ color, size }) => {
         let iconName = '';
- 
+
         if (route.name === 'Recherche') {
           iconName = 'search';
         } else if (route.name === 'Calendrier') {
@@ -38,39 +38,40 @@ const TabNavigator = () => {
         } else if (route.name === 'Messages') {
           iconName = 'comments'
         }
- 
+
         return <FontAwesome name={iconName} size={size} color={color} />
-        ;
+          ;
       },
       tabBarActiveTintColor: '#ffa500',
       tabBarInactiveTintColor: '#000080',
       // tabBarColor: '#121C6E',
       headerShown: false,
     })}
-    tabBarOptions={{
-      tabBarStyle: { backgroundColor: '#121C6E' }, 
-    }}
+      tabBarOptions={{
+        tabBarStyle: { backgroundColor: '#121C6E' },
+      }}
     >
-      
-        <Tab.Screen name="Calendrier" component={CalendarScreen} />
-        <Tab.Screen name="Recherche" component={HomeScreen} />
-        <Tab.Screen name="Créer" component={CreateScreen} />
-        <Tab.Screen name="Messages" component={MessagesListScreen} />
+
+      <Tab.Screen name="Calendrier" component={CalendarScreen} />
+      <Tab.Screen name="Recherche" component={HomeScreen} />
+      <Tab.Screen name="Créer" component={CreateScreen} />
+      <Tab.Screen name="Messages" component={MessagesListScreen} />
     </Tab.Navigator>
   );
 };
 
 export default function App() {
-  return (
-    <Provider store={store}>
-      <NavigationContainer>
-        <Stack.Navigator screenOptions={{ headerShown: false }}>
-          <Stack.Screen name="TabNavigator" component={TabNavigator} />
-          <Stack.Screen name="Profil" component={ProfilScreen} /> 
-          <Stack.Screen name="SignUpJoin" component={SignupJoinScreen} />
-          <Stack.Screen name="SignUpPreferences" component={SignUpPreferencesScreen} />
-        </Stack.Navigator>
-      </NavigationContainer>
-    </Provider>
-  );
+  // return (
+  //   <Provider store={store}>
+  //     <NavigationContainer>
+  //       <Stack.Navigator screenOptions={{ headerShown: false }}>
+  //         <Stack.Screen name="TabNavigator" component={TabNavigator} />
+  //         <Stack.Screen name="Profil" component={ProfilScreen} /> 
+  //         <Stack.Screen name="SignUpJoin" component={SignupJoinScreen} />
+  //         <Stack.Screen name="SignUpPreferences" component={SignUpPreferencesScreen} />
+  //       </Stack.Navigator>
+  //     </NavigationContainer>
+  //   </Provider>
+  // );
+  return <Connection />
 }
