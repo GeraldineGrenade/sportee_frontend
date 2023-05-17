fdca65de6fe5c3db13b3a0b362fbe8830b0b9cf6
Dorian <dorianlelarge@gmail.com>
2023-05-17 18:14:25 +0200

    create component for modal filter

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -2,20 +2,11 @@ import React, { useState } from 'react';
 import { SafeAreaView, View, Text, StyleSheet, TextInput, Button, TouchableOpacity, Modal } from 'react-native'
 import FontAwesome from 'react-native-vector-icons/FontAwesome';
 import ActivityCard from '../components/ActivityCard';
-import Entypo from 'react-native-vector-icons/Entypo';
-import Fontisto from 'react-native-vector-icons/Fontisto';
-import RangeSlider, { Slider } from 'react-native-range-slider-expo'
+import ModalFilter from '../components/ModalFilter';
 
 const HomeScreen = ({ navigation }) => {
-    const [modalVisible, setModalVisible] = useState(false)
     const [map, setMap] = useState(false);
-    const [fromValue, setFromValue] = useState(0);
-    const [toValue, setToValue] = useState(0);
-    const [value, setValue] = useState(0)
-
-    const closeModal = () => {
-        setModalVisible(false);
-    }
+    const [modalVisible, setModalVisible] = useState(false);
 
     return (
         <SafeAreaView style={styles.container}>
@@ -24,52 +15,13 @@ const HomeScreen = ({ navigation }) => {
                 <TouchableOpacity onPress={() => setModalVisible(true)}>
                     <FontAwesome name='align-center' size={25} color='#121C6E' style={styles.filterIcon} />
                 </TouchableOpacity>
+                <ModalFilter modalVisible={modalVisible} setModalVisible={setModalVisible} />
                 <TextInput placeholder='Rechercher une activité' style={styles.input}></TextInput>
                 <View style={styles.userIconContainer}>
                     <FontAwesome name='user' size={25} color='#f8f8ff' style={styles.userIcon} onPress={() => navigation.navigate('Profil')} />
                 </View>
             </View>
-            <Modal
-                animationType='fade'
-                transparent={false}
-                visible={modalVisible}
-                onRequestClose={() => setModalVisible(false)}
-                style={styles.modalFilter}
-            >
-                <View style={styles.topModal}>
-                    <TouchableOpacity onPress={closeModal}>
-                        <Entypo name='cross' size={40} color='#121C6E' style={styles.exitIcon} />
-                    </TouchableOpacity>
-                    <Text>Filtrer</Text>
-                    <TouchableOpacity>
-                        <Text>effacer les filtres</Text>
-                    </TouchableOpacity>
-                </View>
-                <View>
-                    <Text style={styles.searchText}>Où cherches tu ?</Text>
-                </View>
-                <View style={styles.aroundMe}>
-                    <Fontisto name='map-marker-alt' size={25} color='#121C6E' />
-                    <Text style={styles.around}>Autour de moi</Text>
-                </View>
-                <TextInput placeholder='Rechercher une activité' style={styles.searchBar}>
-                    {/* <FontAwesome placeholder='Rechercher une activité' name='search' size={25} color='#D9D9D9' /> */}
-                </TextInput>
-                <View style={styles.slider}>
-                    <Slider min={5} max={100} step={5}
-                        valueOnChange={value => setValue(value)}
-                        initialValue={5}
-                        knobColor='#EA7810'
-                        valueLabelsBackgroundColor='#EA7810'
-                        inRangeBarColor='#000000'
-                        outOfRangeBarColor='#EA7810'
-                        styleSize='small'
-                    />
-                </View>
-                <View>
-                    <Text style={styles.activity}>Quelles activités sportives cherches-tu ?</Text>
-                </View>
-            </Modal>
+
             <View style={styles.iconsNavigate}>
                 <View style={styles.listIconContainer}>
                     <FontAwesome name='list-ul' size={25} color='#121C6E' style={styles.listIcon} />
@@ -100,15 +52,9 @@ const HomeScreen = ({ navigation }) => {
                     <ActivityCard />
                     <ActivityCard />
                 </View>
-<<<<<<< HEAD
-            {/* </View> */}
-        </View>
-            <Button onPress={navigation.navigate('SignUpPreferences')} title="Sign Up"/>  
-=======
                 {/* </View> */}
             </View>
             {/* <Button onPress={navigation.navigate('SignUpPreferences')} title="Sign Up"/>                     */}
->>>>>>> doudou
 
         </SafeAreaView >
     )
@@ -244,58 +190,5 @@ const styles = StyleSheet.create({
         alignItems: 'center',
         overflow: 'scroll',
     },
-    modalFilter: {
-        flex: 1
-    },
-    topModal: {
-        flexDirection: 'row',
-        justifyContent: 'space-between',
-        marginTop: 50,
-        alignItems: 'center',
-        textAlign: 'center',
-    },
-    searchText: {
-        color: '#121C6E',
-        fontWeight: 'bold',
-        fontSize: 20,
-        marginTop: 20,
-        marginLeft: 15
-    },
-    aroundMe: {
-        flexDirection: 'row',
-        marginTop: 20,
-        textAlign: 'center',
-        alignItems: 'center',
-        marginLeft: 15
-    },
-    around: {
-        marginLeft: 15
-    },
-
-    searchBar: {
-        width: '60%',
-        borderColor: '#D9D9D9',
-        borderWidth: 1,
-        borderRadius: 7,
-        fontSize: 16,
-        height: '5%',
-        alignItems: 'center',
-        justifyContent: 'center',
-        marginTop: '5%',
-        textAlign: 'center',
-        marginLeft: '20%'
-    },
-    slider: {
-        marginTop: '0%',
-        zIndex: 999
-    },
-    activity: {
-        color: '#121C6E',
-        fontWeight: 'bold',
-        fontSize: 20,
-        marginTop: '25%',
-        textAlign: 'center'
-    }
-
 });
 
