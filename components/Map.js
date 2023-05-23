552e2772aa5e1ae080b5b4723c5a9c7ae853e307
Josephine <josephine.modiano@gmail.com>
2023-05-23 15:25:58 +0200

    right version

--- components/Map.js
+++ components/Map.js
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import { View, StyleSheet, Modal, Text, TouchableOpacity, Image } from 'react-native'
 import {Dimensions} from 'react-native';
+import moment from 'moment';
+import 'moment/locale/fr'
 import { useDispatch, useSelector } from 'react-redux';
 import MapView, { Marker } from 'react-native-maps';
 import * as Location from 'expo-location';
@@ -12,10 +14,11 @@ const Map = (props) => {
 const [currentPosition, setCurrentPosition] = useState({});
 const [modalVisible, setModalVisible] = useState(false);
 const [pinIconColor, setPinIconColor] = useState('#00bfff');
-const [currentMarker, setCurrentMarker] = useState({});
+const [currentMarker, setCurrentMarker] = useState(null);
+const [dateString, setDateString] = useState('')
+const [timeString, setTimeString] = useState('')
 const activityData = useSelector((state) => state.activities.value)
 const preferences = useSelector((state) => state.preferences.value)
-
 let dispatch = useDispatch()
 
     useEffect(() => {
@@ -32,6 +35,9 @@ let dispatch = useDispatch()
         })();
        }, []);
 
+console.log(currentMarker?.date)
+moment.locale('fr')
+
      const handleOpenPopUp = (data) => {
          setCurrentMarker(data)
          setModalVisible(true);
@@ -43,9 +49,6 @@ let dispatch = useDispatch()
         setPinIconColor('#00bfff')
       };
 
-      // {"__v": 0, "_id": "646b8b896fcac6675b6a961b", "conversation": {"_id": "646b8b896fcac6675b6a961d", "messages": [[Object], [Object], [Object], [Object], [Object], [Object], [Object], [Object], [Object], [Object], [Object], [Object], [Object], [Object], [Object], [Object], [Object], [Object], [Object], [Object], [Object], [Object], [Object], [Object], [Object], [Object]], "users": ["64662968107a0fa2af912a63", "646792f6dea8baa635ef57f5", "64679415f1924a00483f370c", "646794e2dea8baa635ef581b"]}, "date": "2023-05-28T15:29:33.133Z", "description": "Occaecat proident magna reprehenderit et officia pariatur nisi tempor est velit elit sunt.", "handleClickActivityCard": [Function handleClickActivityCard], "level": "Débutant", "name": "Wonderful Event", "nbMaxParticipants": 10, "participants": [{"_id": "646b8b896fcac6675b6a9638", "isApproved": true, "user": "64662968107a0fa2af912a63"}, {"_id": "646b8b896fcac6675b6a9639", "isApproved": false, "user": "646792f6dea8baa635ef57f5"}, {"_id": "646b8b896fcac6675b6a963a", "isApproved": false, "user": "646794e2dea8baa635ef581b"}], "place": {"_id": "646b8b896fcac6675b6a961c", "address": "Nice, Maritime Alps, France", "coords": {"latitude": 43.7009358, "longitude": 7.2683912}}, "sport": {"_id": "646391fc0efb12e60cbd26ad", "icon": "https://res.cloudinary.com/dsd7uux0v/image/upload/v1684246193/sportee/fencing_lm4fsz.png", "name": "escrime", "photo": "https://res.cloudinary.com/dube2vhtq/image/upload/v1684682688/escrime_akfh0l.jpg"}, "time": 3, "user": "64679415f1924a00483f370c"}
-
-
      const allSportMarkers = activityData.map((data, i) => {
      return <Marker key={i} coordinate={{ latitude: data.place.coords.latitude, longitude: data.place.coords.longitude }} pinColor={pinIconColor} onPress={() => handleOpenPopUp({...data})}/>;
    });
@@ -53,26 +56,26 @@ let dispatch = useDispatch()
     return (
        <View>
         <TouchableOpacity>
-         <Modal visible={modalVisible} animationType="fade" transparent>
+        {currentMarker && <Modal visible={modalVisible} animationType="fade" transparent>
            <View style={styles.bottomView}>
            <View style={styles.modalView}>
                <View style={styles.photoContainer}>          
-                 <Image style={styles.sportPhoto} source={require('../assets/sport-photos/surf.jpg')}/>
+                 <Image style={styles.sportPhoto} src={currentMarker.sport.photo}/>
                </View>
            <View style={styles.infoButtonContainer}>
              <View style={styles.infosContainer}>
              <Text style={styles.activityTitle}>{currentMarker.name}</Text>
                  <View style={styles.locInfos}>
                    <FontAwesome name='map-pin' size={16} color='#121C6E' style={styles.mapIcon} />
-                   <Text style={styles.city}>Lille</Text>
+                   <Text style={styles.city}>{currentMarker.place.address}</Text>
                  </View>
                <View style={styles.dateInfos}>
                          <FontAwesome5 name='calendar-alt' size={16} color='#121C6E' style={styles.calendarIcon} />
-                         <Text style={styles.date}>{currentMarker.date}</Text> 
+                         <Text style={styles.date}>{moment(currentMarker.date).format('LLL')}</Text> 
                      </View>
                  <View style={styles.nbrContainer}>
                          <FontAwesome name='user' size={16} color='#121C6E' style={styles.calendarIcon} />
-                         <Text style={styles.nbrParticipants}>Participants 2/{currentMarker.nbMaxParticipants}</Text> 
+                         <Text style={styles.nbrParticipants}>Participants {currentMarker.participants.length}/{currentMarker.nbMaxParticipants}</Text> 
                  </View>
              </View>
              <TouchableOpacity onPress={() => handleClosePopUp()} style={styles.button} activeOpacity={0.8}>
@@ -82,6 +85,7 @@ let dispatch = useDispatch()
            </View>
          </View>
        </Modal>
+} 
      </TouchableOpacity>
 
 
