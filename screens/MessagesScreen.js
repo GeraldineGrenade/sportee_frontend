0202e5fe2cf56a18de5d2c9b0458ec6ca96a967a
Dorian <dorianlelarge@gmail.com>
2023-05-24 15:41:13 +0200

    fetch route get and put for chat

--- screens/MessagesScreen.js
+++ screens/MessagesScreen.js
@@ -9,46 +9,99 @@ import Pusher from 'pusher-js/react-native'
 const MessagesScreen = ({ navigation, route }) => {
     const [messages, setMessages] = useState([])
     const connectedUser = useSelector((state) => state.user.value)
-    // const activityId = useSelector((state) => state.activity.id);
+    const activityId = useSelector((state) => state.activity.id);
 
     useEffect(() => {
+        fetch('https://sportee-backend.vercel.app/conversation/join-channel', {
+            method: 'PUT'
+        })
+            .then(response => response.json())
+            .then(data => console.log('data', data))
+
         const pusher = new Pusher("11d41dded094302fda2e", {
-            cluster: "81666e768eb7907f1a19",
+            cluster: "eu",
             encrypted: true,
         })
         // const channelName = `activity-${activityId}`
         const channel = pusher.subscribe('sportee_channel')
-
-        channel.bind('new-message', () => {
-            // const newMessage = {
-            //     _id: data.id,
-            //     text: data.text,
-            //     createdAt: new Date(data.createdAt),
-            //     user: {
-            //         _id: data.userId,
-            //         name: data.userName,
-            //         avatar: data.userAvatar,
-            //     },
-            // }
-            console.log('message send')
-            // fetch get => giftedchat.append ....
-            // setMessages((previousMessages) =>
-            //     GiftedChat.append(previousMessages, [newMessage])
-            // )
+        channel.bind('pusher:subscription_succeeded', () => {
+            channel.bind('new-message', handleReceiveMessage)
         })
+        //channel.bind('new-message', () => {
+        // const newMessage = {
+        //     _id: data.id,
+        //     text: data.text,
+        //     createdAt: new Date(data.createdAt),
+        //     user: {
+        //         _id: data.userId,
+        //         name: data.userName,
+        //         avatar: data.userAvatar,
+        //     },
+        // }
+        //return 
+        // fetch(`https://sportee-backend.vercel.app/conversation/getConversation/${id}`)
+        //     .then(response => response.json())
+        //     .then(data => {
+        //         if (data.result) {
+        //             setMessages(previousMessages =>
+        //                 GiftedChat.append(previousMessages, data.messages)
+        //             )
+        //         } else {
+        //             console.log('conversation not found')
+        //         }
+        //     })
+        //     .catch(error => {
+        //         console.log(error)
+        //     })
+        // fetch get => giftedchat.append ....
+        // setMessages((previousMessages) =>
+        //     GiftedChat.append(previousMessages, [newMessage])
+        // )
+        // })
 
         return () => {
-            pusher.unsubscribe('channelName')
+            pusher.unsubscribe('sportee_channel')
         }
     }, [])
 
+    const handleReceiveMessage = () => {
+        console.log('super message send')
+        // fetch get messages ici
+        fetch(`https://sportee-backend.vercel.app/conversation/getConversation/${id}`)
+            .then(response => response.json())
+            .then(data => {
+                if (data.result) {
+                    setMessages(previousMessages =>
+                        GiftedChat.append(previousMessages, data.messages)
+                    )
+                } else {
+                    console.log('conversation not found')
+                }
+            })
+            .catch(error => {
+                console.log(error)
+            })
+    }
+
     const onSend = (newMessages = []) => {
-        setMessages((previousMessages) =>
+        setMessages(previousMessages =>
             GiftedChat.append(previousMessages, newMessages)
         )
-        //fetch put ('')
-        const message = newMessages[0]
-    }
+        fetch(`https://sportee-backend.vercel.app/conversation/updateConversation/${activityId}`, {
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(updatedData),
+        })
+            .then(response => response.json())
+            .then(data => {
+                console.log(data);
+            })
+            .catch(error => {
+                console.log(error);
+            });
+    };
 
 
     return (
